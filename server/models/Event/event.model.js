159fe78b273785a7e354c580d2d4442e5024ad14
Arij1998 <arij.zitouni@esprit.tn>
2022-04-09 05:12:45 +0100

    last edit

--- server/models/Event/event.model.js
+++ server/models/Event/event.model.js
@@ -5,8 +5,8 @@ var EventSchema = mongoose.Schema(
         EventName: { type: String, required: true },
         EventDescription: { type: String, required: true },
         picture: { type: String, required: true },
-        StartDate: { type: String, required: true },
-        EndDate: { type: String, required: true },
+        StartDate: { type: Date, required: true },
+        EndDate: { type: Date, required: true },
     },
     { timestamps: true },
 );
