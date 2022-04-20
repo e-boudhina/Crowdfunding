e8044fe4b76358fbe5967b958f318c4b333edd67
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2022-04-20 16:04:18 +0100

    Small change

--- server/middlewares/Service/verifyUserRequestValidity.js
+++ server/middlewares/Service/verifyUserRequestValidity.js
@@ -6,7 +6,8 @@ const userRequest = require ('../../models/Services/userRequest')
 
 const verify_UserRequestValidity =  asyncHandler(async (req, res, next) => {
     // we are setting this as an argument that does not belong to the body, headers or params which a new thing that I learned you can set anything into the request in a middleware
-    let userid = req.headers["userid"];
+    // let userid = req.headers["userid"];
+    let userid = req.userId;
     if (!userid) {
         return res.status(403).send({ message: "Please make sure that you include a valid userid in you request !" });
     }
@@ -16,14 +17,14 @@ const verify_UserRequestValidity =  asyncHandler(async (req, res, next) => {
 
     //if not empty
     if(userRequests) {
-            for(uR of userRequests){
-                //You can not submit a new request unless an your previous request is either Approved or rejected by an Incubator
-                //console.log(uR.status)
-                if (uR.status === undefined)
-                {
-                    return res.status(200).send({ message: "Your previous request is still pending!" });
-                }
+        for(uR of userRequests){
+            //You can not submit a new request unless an your previous request is either Approved or rejected by an Incubator
+            //console.log(uR.status)
+            if (uR.status === undefined)
+            {
+                return res.status(200).send({ message: "Your previous request is still pending!" });
             }
+        }
 
     }
     //else proceed without checking
